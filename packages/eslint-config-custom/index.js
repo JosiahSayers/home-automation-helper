37c9eeb9217ee6f7891f5ed96e39c3d144367ade
Josiah Sayers <josiah.sayers15@gmail.com>
2022-11-22 18:35:24 -0500

    move to turbo repo

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "turbo", "prettier"],
+  rules: {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { "ignoreRestSiblings": true }
+    ]
+  },
+};
