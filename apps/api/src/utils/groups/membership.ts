69ef36e3c6af0584e80c52945e5f33a46a278eaa
Josiah Sayers <josiah.sayers15@gmail.com>
2022-11-23 18:58:52 -0500

    remove .env

--- apps/api/src/utils/groups/membership.ts
+++ apps/api/src/utils/groups/membership.ts
@@ -1,11 +1,48 @@
+import { User } from '@prisma/client';
+import { db } from '../db';
+import { getGroupWithMembers } from './queries';
+
 export const inviteUserToGroup = async (groupId: string, inviterId: string, inviteeEmail: string): Promise<boolean> => {
-  // figure out if inviter has access to group
-  // figure out if invitee is in group
-  // setup group invite record
-  // figure out if invitee has an account
-  // - No account
-  //     send email inviting user to create an account
-  // - Account
-  //     Send push notification about group invite if available
-  //     Send email about group invite if push not available
+  const inviter = await db.user.findUnique({ where: { id: inviterId } });
+  if (!inviter) {
+    return false;
+  }
+  
+  const invitee = await db.user.findUnique({ where: { email: inviteeEmail } });
+  if (!invitee) {
+    return inviteUserWithoutAccount(groupId, inviter, inviteeEmail);
+  }
+
+  const group = await getGroupWithMembers(groupId, inviterId);
+  const userIsAlreadyInGroup = group?.members.some(member => member.userId === invitee.id);
+  if (userIsAlreadyInGroup) {
+    return true;
+  }
+
+  return inviteUserWithAccount(groupId, inviter, invitee);
+};
+
+const inviteUserWithoutAccount = async (groupId: string, inviter: User, inviteeEmail: string) => {
+  const invite = await db.groupInvite.create({
+    data: {
+      groupId,
+      invitedById: inviter.id,
+      email: inviteeEmail
+    }
+  });
+  // send email inviting user to create an account
+  return true;
+};
+
+const inviteUserWithAccount = async (groupId: string, inviter: User, invitee: User) => {
+  const invite = await db.groupInvite.create({
+    data: {
+      groupId,
+      invitedById: inviter.id,
+      userId: invitee.id,
+    }
+  });
+  // send push notification about group invite if available
+  // send email about group invite if push not available
+  return true;
 };
