4713219661c588aa683554cc8b77836231376086
Josiah Sayers <josiah.sayers15@gmail.com>
2022-12-09 09:14:57 -0500

    add remaining tests for group

--- apps/api/api-tests/group.test.ts
+++ apps/api/api-tests/group.test.ts
@@ -2,6 +2,8 @@ import { User } from '@prisma/client';
 import { TRPCError } from '@trpc/server';
 import { expect, it, describe, afterEach, beforeEach } from 'vitest';
 import { trpcRouter } from '../src/app';
+import { db } from '../src/utils/db';
+import { createGroup } from '../src/utils/groups/queries';
 import { createTestUser } from '../src/utils/test-helpers/data';
 import { deleteGroups, deleteUsers } from '../src/utils/test-helpers/teardown';
 
@@ -10,6 +12,7 @@ describe('group.', () => {
   let caller;
   let protectedCaller;
   const groupsToDelete = [];
+  const usersToDelete = [];
   beforeEach(async () => {
     testUser = await createTestUser();
     caller = trpcRouter.createCaller({});
@@ -20,7 +23,7 @@ describe('group.', () => {
     if (groupsToDelete.length > 0) {
       await deleteGroups(groupsToDelete);
     }
-    await deleteUsers([testUser.id]);
+    await deleteUsers([testUser.id, ...usersToDelete]);
   });
 
   describe('create', () => {
@@ -33,6 +36,7 @@ describe('group.', () => {
         expect(false).toBe(true);
       } catch (e) {
         expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('UNAUTHORIZED');
       }
     });
 
@@ -48,6 +52,172 @@ describe('group.', () => {
           description: 'test-group-description',
         })
       );
+    });
+  });
+
+  describe('forCurrentUser', () => {
+    it('requires authentication', async () => {
+      try {
+        await caller.group.forCurrentUser();
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('UNAUTHORIZED');
+      }
+    });
+
+    it('returns an empty array when the user is not a member of any groups', async () => {
+      const response = await protectedCaller.group.forCurrentUser();
+      expect(response).toEqual([]);
+    });
+
+    it('returns the groups that the user is a member of', async () => {
+      const ownerGroup = await createGroup(
+        { name: 'test owner group' },
+        testUser.id
+      );
+      groupsToDelete.push(ownerGroup.id);
+      const otherUser = await createTestUser();
+      usersToDelete.push(otherUser.id);
+      const nonOwnerGroup = await createGroup(
+        { name: 'test non-owner group' },
+        otherUser.id
+      );
+      groupsToDelete.push(nonOwnerGroup.id);
+      await db.groupMembers.create({
+        data: {
+          groupId: nonOwnerGroup.id,
+          userId: testUser.id,
+          membershipType: 'member',
+        },
+      });
+
+      const response = await protectedCaller.group.forCurrentUser();
+      expect(response.length).toBe(2);
+      expect(response.find((g) => g.id === ownerGroup.id)).toBeDefined();
+      expect(response.find((g) => g.id === nonOwnerGroup.id)).toBeDefined();
+    });
+  });
+
+  describe('getById', () => {
+    it('requires authentication', async () => {
+      try {
+        await caller.group.getById({
+          groupId: 'test-group-id',
+        });
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('UNAUTHORIZED');
+      }
+    });
+
+    it('throws an error when the group ID is not found', async () => {
+      try {
+        await protectedCaller.group.getById({
+          groupId: 'test-group-id',
+        });
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('NOT_FOUND');
+      }
+    });
+
+    it('throws an error when the group exists but the calling user is not a member', async () => {
+      try {
+        const group = await createGroup({ name: 'test-group' }, testUser.id);
+        groupsToDelete.push(group.id);
+        await db.groupMembers.deleteMany({
+          where: { groupId: group.id, userId: testUser.id },
+        });
+        await protectedCaller.group.getById({
+          groupId: group.id,
+        });
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('NOT_FOUND');
+      }
+    });
+
+    it('returns the group when it is found and the calling user is a member', async () => {
+      const group = await createGroup({ name: 'test-group' }, testUser.id);
+      groupsToDelete.push(group.id);
+      const response = await protectedCaller.group.getById({
+        groupId: group.id,
+      });
+      expect(response).toEqual(
+        expect.objectContaining({
+          id: group.id,
+          name: group.name,
+          description: group.description,
+          members: [
+            {
+              userId: testUser.id,
+              membershipType: 'owner',
+              user: {
+                name: testUser.name,
+              },
+            },
+          ],
+        })
+      );
+    });
+  });
+
+  describe.only('update', () => {
+    it('requires authentication', async () => {
+      try {
+        await caller.group.update({
+          groupId: '',
+        });
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('UNAUTHORIZED');
+      }
+    });
+
+    it('returns an error when the user is not a group owner', async () => {
+      const group = await createGroup({ name: 'test group' }, testUser.id);
+      groupsToDelete.push(group.id);
+      try {
+        await db.groupMembers.updateMany({
+          where: { groupId: group.id, userId: testUser.id },
+          data: { membershipType: 'member' },
+        });
+        await protectedCaller.group.update({
+          groupId: group.id,
+          name: 'updated name',
+        });
+        expect(false).toBe(true);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TRPCError);
+        expect(e.code).toBe('FORBIDDEN');
+        const groupAfterCall = await db.group.findUnique({
+          where: { id: group.id },
+        });
+        expect(groupAfterCall.name).toBe('test group');
+      }
+    });
+
+    it('allows owners to update a group', async () => {
+      const group = await createGroup(
+        { name: 'test group', description: 'test description' },
+        testUser.id
+      );
+      groupsToDelete.push(group.id);
+      await protectedCaller.group.update({
+        groupId: group.id,
+        name: 'updated name',
+        description: 'updated description',
+      });
+      const groupAfterUpdate = await db.group.findUnique({
+        where: { id: group.id },
+      });
+      expect(groupAfterUpdate.name).toBe('updated name');
+      expect(groupAfterUpdate.description).toBe('updated description');
     });
   });
 });
